e31e10b52ee808ca94ac3095b8e6b82970132645
gabriellapendo7 <gabriellapendo12@gmail.com>
2024-06-13 08:27:19 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci_sequence = []
+    
+    for i in range(length):
+        if i == 0:
+            fibonacci_sequence.append(0)
+        elif i == 1:
+            fibonacci_sequence.append(1)
+        else:
+            next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+            fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
+# Example usage
+print_fibonacci(9)
